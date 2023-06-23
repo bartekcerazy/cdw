050cb78f637de90a5e32125b71467b12b999b216
bartekcerazy <bartekcerazy@gmail.com>
2023-06-23 21:37:20 +0200

    dodano dokuemntacje do operacje programu kalkulator

--- untitled/src/main/java/pakiet1/Operacje.java
+++ untitled/src/main/java/pakiet1/Operacje.java
@@ -1,14 +1,35 @@
 package pakiet1;
 
 public class Operacje {
+    /**
+     * Dodaje dwie liczby.
+     *
+     * @param a pierwsza liczba
+     * @param b druga liczba
+     * @return suma dwóch liczb
+     */
     public double dodaj(double a, double b) {
         return a + b;
     }
 
+    /**
+     * Odejmuje jedną liczbę od drugiej.
+     *
+     * @param a liczba, od której odejmujemy
+     * @param b liczba, którą odejmujemy
+     * @return różnica między dwiema liczbami
+     */
     public double odejmij(double a, double b) {
         return a - b;
     }
 
+    /**
+     * Mnoży dwie liczby.
+     *
+     * @param a pierwsza liczba
+     * @param b druga liczba
+     * @return iloczyn dwóch liczb
+     */
     public double pomnoz(double a, double b) {
         return a * b;
     }
