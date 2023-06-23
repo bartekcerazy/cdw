a214468903e558adf2d2071dc1f646982b486b90
bartekcerazy <bartekcerazy@gmail.com>
2023-06-23 21:36:14 +0200

    dodano dokuemntacje do maina programu kalkulator

--- untitled/src/main/java/pakiet1/Main.java
+++ untitled/src/main/java/pakiet1/Main.java
@@ -1,8 +1,17 @@
 package pakiet1;
 
+import org.apache.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    /**
+     * Metoda główna programu.
+     *
+     * @param args argumenty wiersza poleceń
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -25,17 +34,21 @@ public class Main {
             case 1:
                 double suma = kalkulator.dodaj(liczba1, liczba2);
                 System.out.println("Suma: " + suma);
+                logger.info("Suma: " + suma);
                 break;
             case 2:
                 double roznica = kalkulator.odejmij(liczba1, liczba2);
                 System.out.println("Różnica: " + roznica);
+                logger.info("Różnica: " + roznica);
                 break;
             case 3:
                 double iloczyn = kalkulator.pomnoz(liczba1, liczba2);
                 System.out.println("Iloczyn: " + iloczyn);
+                logger.info("Iloczyn: " + iloczyn);
                 break;
             default:
                 System.out.println("Nieprawidłowy wybór operacji.");
+                logger.warn("Nieprawidłowy wybór operacji.");
         }
 
         scanner.close();
