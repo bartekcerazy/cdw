37fe599bb63e364d5ea5d9262a4ddd5402d41509
bartekcerazy <bartekcerazy@gmail.com>
2023-06-23 21:32:25 +0200

    dodano dokuemntacje do kalkulatora oraz zmieniono pom.xml aby dzialalo log4

--- untitled/src/main/java/pakiet1/Kalkulator.java
+++ untitled/src/main/java/pakiet1/Kalkulator.java
@@ -1,21 +1,52 @@
+
 package pakiet1;
+
+import org.apache.log4j.Logger;
 
 public class Kalkulator {
     private final Operacje operacje;
+    private static final Logger logger = Logger.getLogger(Kalkulator.class);
 
     public Kalkulator() {
         operacje = new Operacje();
     }
 
+    /**
+     * Dodaje dwie liczby.
+     *
+     * @param a pierwsza liczba
+     * @param b druga liczba
+     * @return suma dwóch liczb
+     */
     public double dodaj(double a, double b) {
-        return operacje.dodaj(a, b);
+        double suma = operacje.dodaj(a, b);
+        logger.info("Dodawanie: " + a + " + " + b + " = " + suma);
+        return suma;
     }
 
+    /**
+     * Odejmuje jedną liczbę od drugiej.
+     *
+     * @param a liczba, od której odejmujemy
+     * @param b liczba, którą odejmujemy
+     * @return różnica między dwiema liczbami
+     */
     public double odejmij(double a, double b) {
-        return operacje.odejmij(a, b);
+        double roznica = operacje.odejmij(a, b);
+        logger.info("Odejmowanie: " + a + " - " + b + " = " + roznica);
+        return roznica;
     }
 
+    /**
+     * Mnoży dwie liczby.
+     *
+     * @param a pierwsza liczba
+     * @param b druga liczba
+     * @return iloczyn dwóch liczb
+     */
     public double pomnoz(double a, double b) {
-        return operacje.pomnoz(a, b);
+        double iloczyn = operacje.pomnoz(a, b);
+        logger.info("Mnożenie: " + a + " * " + b + " = " + iloczyn);
+        return iloczyn;
     }
 }
