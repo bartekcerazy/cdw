7575510bb573d895183a10296217b92f6a24bc3b
bartekcerazy <bartekcerazy@gmail.com>
2023-06-23 21:39:30 +0200

    dodano dokuemntacje do contact.manager programu o kontaktach

--- untitled/src/main/java/pakiet2/ContactManager.java
+++ untitled/src/main/java/pakiet2/ContactManager.java
@@ -2,16 +2,27 @@ package pakiet2;
 
 import java.util.ArrayList;
 import java.util.List;
+
 public class ContactManager {
     private final List<Contact> contacts = new ArrayList<>();
 
     public ContactManager() {
     }
 
+    /**
+     * Dodaje kontakt do listy kontaktów.
+     *
+     * @param contact kontakt do dodania
+     */
     public void addContact(Contact contact) {
         contacts.add(contact);
     }
 
+    /**
+     * Zwraca listę kontaktów.
+     *
+     * @return lista kontaktów
+     */
     public List<Contact> getContacts() {
         return contacts;
     }
